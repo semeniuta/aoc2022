faff3ec08d35de21e0085b9f8d78cc1765a57790
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2022-12-09 23:20:34 +0100

    day 9 second (& fix)

--- day_09/rope.py
+++ day_09/rope.py
@@ -11,16 +11,45 @@ class Rope:
 
         for _ in range(length):
 
-            head_prev = self.head.copy()
             self.head += dir_vec
             
-            dx, dy = np.abs(self.head - self.tail)
+            diff = self.head - self.tail
+            diff_abs = np.abs(diff)
 
-            if (dx == 1 and dy == 0) or (dx == 0 and dy == 1) or (dx == 1 and dy == 1) or (dx == 0 and dy == 0):
-                continue
+            diff_max = np.max(diff_abs)
+            diff_min = np.min(diff_abs)
 
-            self.tail = head_prev
+            if np.abs(diff_max) == 1 or np.abs(diff_max) == 0:
+                 continue
+
+            if diff_max == 2 and diff_min == 0:
+                self.tail += np.array(diff / 2, dtype=int)
+            elif diff_max == 2 and (diff_min == 1 or diff_min == 2):
+                movement = np.array([val / 2 if abs(val) == 2 else val for val in diff], dtype=int)
+                self.tail += movement
+            else:
+                raise Exception("unreachable")
+
             self.visited.add(tuple(self.tail))
+
+
+class LongRope:
+
+    def __init__(self):
+        self.parts = [Rope() for _ in range(9)]
+
+    def move(self, dir_vec, length):
+
+        for _ in range(length):
+
+            self.parts[0].move(dir_vec, 1)
+
+            for i in range(1, 9):
+                current = self.parts[i]
+                previous = self.parts[i - 1]
+
+                dir_vec_current = previous.tail - current.head
+                current.move(dir_vec_current, 1)
 
 
 def parse_command(line):
@@ -53,4 +82,11 @@ if __name__ == '__main__':
 
     print(len(sim.visited))
 
-        
+    with open('data/input.txt') as f:
+        commands = [parse_command(line) for line in f]
+
+        big_sim = LongRope()
+        for dir_vec, length in commands:
+            big_sim.move(dir_vec, length)
+
+    print(len(big_sim.parts[-1].visited))
